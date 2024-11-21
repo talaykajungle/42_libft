55c57751a7999d20184ee64e8be13bb5fbefbec7
Talha <mutlutalha02@gmail.com>
2024-11-21 19:16:28 +0100

    finished ft_strlcpy.c started ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,8 +6,26 @@
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:01:53 by tamutlu           #+#    #+#             */
-/*   Updated: 2024/11/13 19:52:48 by tamutlu          ###   ########.fr       */
+/*   Updated: 2024/11/21 17:00:06 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t i;
+
+	i = 0;
+
+	if (size > 0)
+	{
+		while (dest > i)
+		{
+			
+		}
+		
+	}
+	
+	
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 13:05:34 by tamutlu           #+#    #+#             */
-/*   Updated: 2024/11/20 21:32:30 by tamutlu          ###   ########.fr       */
+/*   Updated: 2024/11/21 15:59:18 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ int					ft_tolower(int c);
 int					ft_toupper(int c);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memset(void *s, int c, size_t n);
+size_t 				ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
