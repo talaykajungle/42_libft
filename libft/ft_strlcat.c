c7f408c1762e3be4ba6ec480180d33880ab8b5ff
Talha <mutlutalha02@gmail.com>
2024-11-25 21:18:34 +0100

    Started ft_strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,26 +6,52 @@
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 19:01:53 by tamutlu           #+#    #+#             */
-/*   Updated: 2024/11/21 17:00:06 by tamutlu          ###   ########.fr       */
+/*   Updated: 2024/11/25 20:07:11 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dest, const char *src, size_t size)
+size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t i;
+	size_t dstlen;
+	size_t srclen;
 
+// Calculating the length of the strings
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+
+// Check for buffer size (Block of memory that is storen temporarily that will be transferred)
+	if(size < dstlen)
+		return(size + srclen);
 	i = 0;
-
-	if (size > 0)
+// Append characters need to use a loop
+	while(src[i] != '\0' && (dstlen + i) < (size -1))
 	{
-		while (dest > i)
-		{
-			
-		}
-		
+		dst[dstlen + i] = src[i];
+		i++;
 	}
-	
-	
+	dst[dstlen + i] = '\0';
+	return(dstlen + srclen);
+}
+
+int main()
+{
+// Empty destination string
+	char dst[10] = "";
+	char *src = "Hello";
+	size_t result = ft_strlcat(dst, src, 10);
+	// Expected: dst = "Hello", result = 5
+
+// Empty source string
+	char dst[10] = "Hello";
+	char *src = "";
+	size_t result = ft_strlcat(dst, src, 10);
+	// Expected: dst = "Hello", result = 5
+
+// Size is 0
+	char dest[10] = "Hello";
+	char *src = "";
+	size_t result = ft_strlcat(dest, src, 10);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 13:05:34 by tamutlu           #+#    #+#             */
-/*   Updated: 2024/11/21 15:59:18 by tamutlu          ###   ########.fr       */
+/*   Updated: 2024/11/25 16:43:24 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int					ft_toupper(int c);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memset(void *s, int c, size_t n);
 size_t 				ft_strlcpy(char *dest, const char *src, size_t size);
+size_t				ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
